e61ea98d0c0edfe60e6a4f29336845694dc24aca
arklem699 <jrsy.tdh@yandex.ru>
2024-04-16 11:16:08 +0300

    update function indexing_links

--- indexing_link/views.py
+++ indexing_link/views.py
@@ -3,11 +3,14 @@ import subprocess
 
 
 def indexing_links(request):
-    links = request.POST.get('links', '').strip()
-    links = links.split('\n')
-    with open('google-indexing-api-bulk-master/urls.txt', 'w', newline='\n') as f:
-        for link in links:
-            f.write(link + '\n')
-    process = subprocess.Popen(['C:/Program Files/nodejs/node.exe', 'index.js'], stdout=subprocess.PIPE, cwd='google-indexing-api-bulk-master/')
-    output, _ = process.communicate()
-    return render(request, 'index.html', {'output': output.decode()})
+    if request.method == 'POST':
+        links = request.POST.get('links', '').strip()
+        links = links.split('\n')
+        with open('google-indexing-api-bulk-master/urls.txt', 'w', newline='\n') as f:
+            for link in links:
+                f.write(link + '\n')
+        process = subprocess.Popen(['node', 'index.js'], stdout=subprocess.PIPE, cwd='google-indexing-api-bulk-master/')
+        output, _ = process.communicate()
+        return render(request, 'index.html', {'output': output.decode()})
+    else:
+        return render(request, 'index.html', {'output': ''})
